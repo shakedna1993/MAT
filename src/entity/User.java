f1967dbbce2f0c7f9b0d82f066f72f65f9e8d842
Shaked Nagar <shaakedi@gmail.com>
2017-06-04 16:01:42 +0300

    remove shahar changes

--- src/entity/User.java
+++ src/entity/User.java
@@ -9,7 +9,6 @@ public class User implements Serializable  {
 	private static final long serialVersionUID = 1L;
 	private String Name,Password;
 	private int Id,isConnected,priv;
-	private double dd1;
 	
 	public User() {
 		super();
